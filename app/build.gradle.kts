122e533694441abbc62682f210cfc5853982c577
Milan ISAEV <isaev.milan@gmail.com>
2024-05-10 05:03:10 +0300

    task-1 1.Произведены настройки АндроидМанифест: -добавлены разрешения -ориентация зафиксирована портретная -настройка исчезновения боттом бара при открытии клавиатуры
    2.Добавлены зависимости в гредл
    
    3.Тема родительская заменена на Theme.MaterialComponents.DayNight.NoActionBar
    (стили еще не делал)
    
    4.Добавлена Legacy-иконка

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("ru.practicum.android.diploma.plugins.developproperties")
+
 }
 
 android {
@@ -55,4 +56,35 @@ dependencies {
     androidTestImplementation(libs.uiTests.junitExt)
     androidTestImplementation(libs.uiTests.espressoCore)
     // endregion
+
+    implementation("androidx.constraintlayout:constraintlayout-core:1.0.4")
+    implementation("com.android.volley:volley:1.2.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.github.bumptech.glide:glide:4.15.1")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.15.1")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("androidx.activity:activity-ktx:1.9.0")
+    implementation("io.insert-koin:koin-android:3.3.0")
+    implementation("androidx.fragment:fragment-ktx:1.5.5")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("io.reactivex.rxjava2:rxjava:2.2.21")
+    implementation("androidx.room:room-runtime:2.6.1") // библиотека Room
+    implementation("androidx.room:room-ktx:2.6.1")
+    implementation("com.github.tbruyelle:rxpermissions:0.12")
+    implementation("io.reactivex.rxjava3:rxjava:3.1.3")
+    implementation("io.reactivex.rxjava3:rxandroid:3.0.0")
+    implementation("jp.wasabeef:glide-transformations:4.3.0")
+    implementation("com.markodevcic:peko:2.1.2")
+    implementation("androidx.lifecycle:lifecycle-livedata-core-ktx:2.7.0")
+
 }
