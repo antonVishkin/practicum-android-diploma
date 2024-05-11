6eeb76e9d6fd7c494f90e81f60d97135c122e31c
Anton Vyshkin <a.vyshkin@tinkoff.ru>
2024-05-11 23:47:37 +0300

    add vacancy module

--- app/src/main/java/ru/practicum/android/diploma/App.kt
+++ app/src/main/java/ru/practicum/android/diploma/App.kt
@@ -7,13 +7,14 @@ import ru.practicum.android.diploma.di.dataModule
 import ru.practicum.android.diploma.di.favoriteModule
 import ru.practicum.android.diploma.di.filtrationModule
 import ru.practicum.android.diploma.di.searchModule
+import ru.practicum.android.diploma.di.vacancyModule
 
 class App:Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
             androidContext(this@App)
-            modules(dataModule, favoriteModule, filtrationModule, searchModule)
+            modules(dataModule, favoriteModule, filtrationModule, searchModule, vacancyModule)
         }
     }
 }
