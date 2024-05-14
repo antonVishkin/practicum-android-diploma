57935332664ad46dfb501593cd4b44798625ef41
Milan ISAEV <isaev.milan@gmail.com>
2024-05-14 03:22:11 +0300

    .Пофиксен RootActivity .Пофиксены переходы по экранам .Добавлена кнопка в тулбар для перехода на экран с фильтрами .Удалены дублирующиеся названия экранов

--- app/src/main/java/ru/practicum/android/diploma/ui/search/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/SearchFragment.kt
@@ -4,12 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
 import androidx.fragment.app.Fragment
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
-import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
