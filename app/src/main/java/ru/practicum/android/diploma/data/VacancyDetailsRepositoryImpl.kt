a8536192a1f20252fd08e19f9dcc67e100693ff4
Milan ISAEV <isaev.milan@gmail.com>
2024-05-22 00:24:29 +0300

    Добавлены интеракторы репозитории

---
+++ app/src/main/java/ru/practicum/android/diploma/data/VacancyDetailsRepositoryImpl.kt
@@ -0,0 +1,34 @@
+package ru.practicum.android.diploma.data
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import ru.practicum.android.diploma.data.dto.DTOConverters
+import ru.practicum.android.diploma.data.dto.VacancyDetailsResponse
+import ru.practicum.android.diploma.data.network.RetrofitNetworkClient
+import ru.practicum.android.diploma.domain.api.details.VacancyDetailsRepository
+import ru.practicum.android.diploma.domain.models.VacancyPage
+
+class VacancyDetailsRepositoryImpl(
+    private val retrofitNetworkClient: RetrofitNetworkClient,
+    private val dTOConverters: DTOConverters
+) : VacancyDetailsRepository {
+    override fun getVacancyDetails(vacancyId: String): Flow<Result<VacancyPage>> = flow {
+        val response = retrofitNetworkClient.doRequestForDetails(vacancyId)
+        when (response.resultCode) {
+            CLIENT_SUCCESS_RESULT_CODE -> {
+                val detailsResponse = response as VacancyDetailsResponse
+                val vacancyDetails = dTOConverters.map(detailsResponse)
+                emit(Result.success(vacancyDetails))
+            }
+
+            else -> {
+                emit(Result.failure(Throwable("Failed to fetch vacancy details")))
+            }
+        }
+    }
+
+    companion object {
+        const val CLIENT_SUCCESS_RESULT_CODE = 200
+    }
+}
+
---
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/VacancyDetailsResponse.kt
@@ -0,0 +1,29 @@
+package ru.practicum.android.diploma.data.dto
+
+data class VacancyDetailsRequest(val vacancyId: String)
+
+data class VacancyDetailsResponse(
+    val id: String,
+    val name: String,
+    val logoUrl: String?,
+    val employer: Employer,
+    val area: Area,
+    val experience: Experience,
+    val description: String,
+    val responsibilities: String,
+    val requirements: String,
+    val conditions: String,
+    val keySkills: List<KeySkill>,
+    val contacts: Contacts,
+    val comments: String?
+) :Response ()
+
+data class Employer(val name: String)
+data class Area(val name: String)
+data class Experience(val name: String)
+data class KeySkill(val name: String)
+data class Contacts(
+    val contactPerson: String,
+    val email: String,
+    val phone: String
+)
