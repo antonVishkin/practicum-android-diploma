ed49bd83e1d3315581e5c62cf4146cd2f68572e8
Anton Vyshkin <a.vyshkin@tinkoff.ru>
2024-05-22 12:46:26 +0300

    - add work with logo to network and view holder

--- app/src/main/java/ru/practicum/android/diploma/data/dto/EmployerDTO.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/EmployerDTO.kt
@@ -1,6 +1,10 @@
 package ru.practicum.android.diploma.data.dto
 
+import com.google.gson.annotations.SerializedName
+
 data class EmployerDTO(
     val id: String?,
-    val name: String
+    val name: String,
+    @SerializedName("logo_urls")
+    val logoUrls: LogoDTO?,
 )
