34a51af20107a1968cb8832de0b9f2f947f29ca1
Kate Mahova <kate.mahova@yandex.ru>
2024-05-26 10:53:24 +0300

    feat: Industry Repository and Interactor

---
+++ app/src/main/java/ru/practicum/android/diploma/domain/impl/industry/IndustryInteractorImpl.kt
@@ -0,0 +1,13 @@
+package ru.practicum.android.diploma.domain.impl.industry
+
+import kotlinx.coroutines.flow.Flow
+import ru.practicum.android.diploma.domain.api.industry.IndustryInteractor
+import ru.practicum.android.diploma.domain.api.industry.IndustryRepository
+import ru.practicum.android.diploma.domain.models.Industry
+import ru.practicum.android.diploma.util.SearchResultData
+
+class IndustryInteractorImpl (private val repository : IndustryRepository): IndustryInteractor {
+    override suspend fun getIndustries(): Flow<SearchResultData<List<Industry>>> {
+        return repository.getIndustries()
+    }
+}
