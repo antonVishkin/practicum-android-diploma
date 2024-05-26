36eef1c841078b9f614947a01af4527d21077674
Kate Mahova <kate.mahova@yandex.ru>
2024-05-26 11:00:47 +0300

    fix: detect

--- app/src/main/java/ru/practicum/android/diploma/domain/impl/industry/IndustryInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/impl/industry/IndustryInteractorImpl.kt
@@ -6,7 +6,7 @@ import ru.practicum.android.diploma.domain.api.industry.IndustryRepository
 import ru.practicum.android.diploma.domain.models.Industry
 import ru.practicum.android.diploma.util.SearchResultData
 
-class IndustryInteractorImpl (private val repository : IndustryRepository): IndustryInteractor {
+class IndustryInteractorImpl(private val repository: IndustryRepository) : IndustryInteractor {
     override suspend fun getIndustries(): Flow<SearchResultData<List<Industry>>> {
         return repository.getIndustries()
     }
