1726baad827e3f4b1e59a242dc34d7268fc252c9
Anton Vyshkin <a.vyshkin@tinkoff.ru>
2024-05-31 10:07:17 +0300

    -fix detekt

--- app/src/main/java/ru/practicum/android/diploma/util/ContactsFormat.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/ContactsFormat.kt
@@ -1,16 +1,20 @@
 package ru.practicum.android.diploma.util
 
 import ru.practicum.android.diploma.domain.models.Phone
+import java.util.Locale
 
 object ContactsFormat {
+    private const val CITY_CODE_LENGTH = 3
+    private const val CITY_NUMBER_PARTS = 2
     fun numberFormat(phone: Phone): String {
         return String.format(
+            Locale.UK,
             "+%1s (%2s) %3s-%4s-%5s",
             phone.country,
             phone.city,
-            phone.number?.dropLast(4),
-            phone.number?.drop(3)?.dropLast(2),
-            phone.number?.drop(5)
+            phone.number?.take(CITY_CODE_LENGTH),
+            phone.number?.dropLast(CITY_NUMBER_PARTS)?.takeLast(CITY_NUMBER_PARTS),
+            phone.number?.takeLast(CITY_NUMBER_PARTS)
         )
     }
 }
