3938dbc32cc168372bef3376ba970b7b393e9148
Anton Vyshkin <a.vyshkin@tinkoff.ru>
2024-06-04 20:30:27 +0300

    - remove unused import

--- app/src/main/java/ru/practicum/android/diploma/ui/vacancies/VacancyDetailsViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/vacancies/VacancyDetailsViewModel.kt
@@ -1,6 +1,5 @@
 package ru.practicum.android.diploma.ui.vacancies
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
